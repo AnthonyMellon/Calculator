1a9dc48972637dc5741c1a846911430d078fca68
AnthonyMellon <Anthonymellon@yahoo.com>
2019-07-24 16:49:18 +1200

    Calculator

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonOpertor = new System.Windows.Forms.Button();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 116);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(324, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 116);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // buttonOpertor
+            // 
+            this.buttonOpertor.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpertor.Location = new System.Drawing.Point(218, 12);
+            this.buttonOpertor.Name = "buttonOpertor";
+            this.buttonOpertor.Size = new System.Drawing.Size(100, 116);
+            this.buttonOpertor.TabIndex = 2;
+            this.buttonOpertor.Text = "+";
+            this.buttonOpertor.UseVisualStyleBackColor = true;
+            this.buttonOpertor.Click += new System.EventHandler(this.buttonOpertor_Click);
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEquals.Location = new System.Drawing.Point(530, 12);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(100, 116);
+            this.buttonEquals.TabIndex = 3;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            this.buttonEquals.Click += new System.EventHandler(this.buttonEquals_Click);
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEnd.Location = new System.Drawing.Point(636, 12);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(200, 116);
+            this.textBoxEnd.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 131);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.buttonOpertor);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonOpertor;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.TextBox textBoxEnd;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public static int Operator = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpertor_Click(object sender, EventArgs e)
+        {
+            char[] operators = {'+', '-', '*', '/', 'M', 'D' };
+            Operator++;
+            Operator = NumLoop(Operator, 0, operators.Length - 1);
+            buttonOpertor.Text = Convert.ToString(operators[Operator]);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            switch (buttonOpertor.Text)
+            {
+                case "+":
+                    textBoxEnd.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+                    break;
+                case "-":
+                    textBoxEnd.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text));
+                    break;
+                case "*":
+                    textBoxEnd.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+                    break;
+                case "/":
+                    textBoxEnd.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text));
+                    break;
+                case "M":
+                    textBoxEnd.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) % Convert.ToDouble(textBox2.Text));
+                    break;
+                case "D":
+                    textBoxEnd.Text = Convert.ToString((Convert.ToInt16(textBox1.Text) / Convert.ToInt16(textBox2.Text) * Convert.ToInt16(textBox2.Text)));
+                    break;
+            }
+        }
+
+        private static int NumLoop(int input, int lower, int upper)
+        {
+            if (input < lower)
+            {
+                input = upper;
+            }
+            else if (input > upper)
+            {
+                input = lower;
+            }
+
+            return input;
+
+        } //Number Loop End
+    }
+}
